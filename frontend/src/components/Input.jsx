ce7f55366735fa5870fdca65c9f2e53ddeed6cb1
Dhruvi Shah <dhruvishah549@gmail.com>
2023-10-07 02:29:16 -0400

    Added basics of form -> not styled, info not sent to db yet

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -4,11 +4,22 @@ import '../css/FrontPage.css'
 import "../css/Interface.css"
 import {useState} from "react";
 
+function CreateNewCase(e) {
+    e.preventDefault();
+    print()
+}
+
 export default function Input() {
     const [formData, setFormData] = useState({
-        textInput: '',
-        numberInput: 0,
-        emailInput: ''
+        firstName: '',
+        lastName: '',
+        emailAddress: '',
+        address: '',
+        phoneNumber: '',
+        birthDate: '',
+        natureOfCase: '', // can be something like contract dispute, felony drug chages, divorce and property divison, will drafting and probate
+        desiredOutcome: '', // can be something like damages, injuntion, custody arrangement
+        dateOfIncident: '',
     });
 
     const handleChange = (event) => {
@@ -17,37 +28,90 @@ export default function Input() {
     };
 
     return (
-        <form className="input">
-            <h2>Input Component</h2>
-            <div>
-                <label>Text Input:</label>
+        <form className="form-container">
+            <h2>New Case</h2>
+            <div className="form-input">
+                <label>First Name: </label>
                 <input
                     type="text"
-                    name="textInput"
-                    value={formData.textInput}
+                    name="firstName"
+                    value={formData.firstName}
                     onChange={handleChange}
                 />
             </div>
-            <div>
-                <label>Number Input:</label>
+            <div className="form-input">
+                <label>Last Name: </label>
                 <input
-                    type="number"
-                    name="numberInput"
-                    value={formData.numberInput}
+                    type="text"
+                    name="lastName"
+                    value={formData.lastName}
                     onChange={handleChange}
                 />
             </div>
-            <div>
-                <label>Email Input:</label>
+            <div className="form-input">
+                <label>Email Address: </label>
                 <input
                     type="email"
-                    name="emailInput"
-                    value={formData.emailInput}
+                    name="emailAddress"
+                    value={formData.emailAddress}
                     onChange={handleChange}
                 />
             </div>
+            <div className="form-input">
+                <label>Address: </label>
+                <input
+                    type="email"
+                    name="emailAddress"
+                    value={formData.emailAddress}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-input">
+                <label>Phone Number: </label>
+                <input
+                    type="text"
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-input">
+                <label>Birthdate: </label>
+                <input
+                    type="date"
+                    name="birthDate"
+                    value={formData.birthDate}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-input">
+                <label>Nature of Case: </label>
+                <input
+                    type="text"
+                    name="natureOfCase"
+                    value={formData.natureOfCase}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-input">
+                <label>Desired Outcome: </label>
+                <input
+                    type="text"
+                    name="desiredOutcome"
+                    value={formData.desiredOutcome}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-input">
+                <label>Date of Incident: </label>
+                <input
+                    type="text"
+                    name="dateOfIncident"
+                    value={formData.dateOfIncident}
+                    onChange={handleChange}
+                />
+            </div>
+            <button onSubmit={CreateNewCase}>Create New Case</button>
         </form>
     );
 }
-
-
