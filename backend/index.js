9b41bf0cba6f301f8137a2799805d00772ed0ffb
Abdullah AL Hinaey <101839293+AbdullahALX@users.noreply.github.com>
2023-10-08 03:27:34 -0400

    changes 3am

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
-import express from "express";
-import cors from "cors"
-import multer from "multer";
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
 const app = express();
 import dotenv from 'dotenv';
-import axios from "axios";
-import OpenAI from "openai";
-import fs from "fs";
-import tmp from "tmp-promise";
-import connectMongo from "./connectMongo.js"
-import mongoRoute from "./mongoRoute.js";
+import axios from 'axios';
+import OpenAI from 'openai';
+import fs from 'fs';
+import tmp from 'tmp-promise';
+import connectMongo from './connectMongo.js';
+import mongoRoute from './mongoRoute.js';
 
 dotenv.config();
 
@@ -16,15 +16,13 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(4000, () => {
-    //conecting to db
-    connectMongo.connectToServer("morgan");
-    console.log("Server Running, Port: 4000")
-})
-
+  //conecting to db
+  connectMongo.connectToServer('morgan');
+  console.log('Server Running, Port: 4000');
+});
 
 const configuration = new OpenAI({
-    apiKey: process.env.CHATGPT,
-
+  apiKey: process.env.CHATGPT,
 });
 
 const openai = new OpenAI(configuration);
@@ -32,29 +30,30 @@ const openai = new OpenAI(configuration);
 //VOICE
 
 const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 app.post('/transcribe', upload.single('audio'), async (req, res) => {
+  console.log('received transcribe');
 
-    console.log("received transcribe")
-
-    try {
-
-        // Create a temporary file to save the buffer
-        const tmpFile = await tmp.file({postfix: '.wav'});
-        // Write the buffer to the temporary file
-        await fs.promises.writeFile(tmpFile.path, req.file.buffer);
-        // Create a read stream from the temporary file
-        const fileStream = fs.createReadStream(tmpFile.path);
-        console.log("Waiting to transcribe all data!")
-        // Use the stream for transcription
-        const transcript = await openai.audio.transcriptions.create({
-            file: fileStream, model: "whisper-1",
-        });
-        const completion = await openai.chat.completions.create({
-            model: "gpt-4", messages: [{
-                role: "user",
-                content: `Given the provided narrative of a client's incident, please structure the information into the following format, but make sure your response is in a stringified json with an attribute "case" for everything below and an attribute "missing" information for any of the fields the client did not provide. Here is the properties for case.  
+  try {
+    // Create a temporary file to save the buffer
+    const tmpFile = await tmp.file({ postfix: '.wav' });
+    // Write the buffer to the temporary file
+    await fs.promises.writeFile(tmpFile.path, req.file.buffer);
+    // Create a read stream from the temporary file
+    const fileStream = fs.createReadStream(tmpFile.path);
+    console.log('Waiting to transcribe all data!');
+    // Use the stream for transcription
+    const transcript = await openai.audio.transcriptions.create({
+      file: fileStream,
+      model: 'whisper-1',
+    });
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-4',
+      messages: [
+        {
+          role: 'user',
+          content: `Given the provided narrative of a client's incident, please structure the information into the following format, but make sure your response is in a stringified json with an attribute "case" for everything below and an attribute "missing" information for any of the fields the client did not provide. Here is the properties for case.  
                  
                            {
                 "contactInformation": {
@@ -79,34 +78,32 @@ app.post('/transcribe', upload.single('audio'), async (req, res) => {
                 
                 If any portion of the narrative is not in English, please translate it. Ensure the information is as accurate and comprehensive as possible based on the provided narrative. If any field or information is missing, do not add that field to "case" instead add the field to the "missing" attribute. The missing attribute should be an array of strings of what's missing. 
                 
-                This is the description from the client: ${transcript.text} REMEMBER YOUR RESPONSE IS IN A STRINGIFIED JSON, DO NOT RESPOND IN ANY OTHER FORMAT!`
-            }]
-        })
+                This is the description from the client: ${transcript.text} REMEMBER YOUR RESPONSE IS IN A STRINGIFIED JSON, DO NOT RESPOND IN ANY OTHER FORMAT!`,
+        },
+      ],
+    });
 
-        console.log(completion.choices[0].message);
+    console.log(completion.choices[0].message);
 
-
-        const parsedData = JSON.parse(JSON.parse(completion.choices[0].message.content));
-        console.log(parsedData)
-        console.log(transcript);
-        res.set('Content-Type', 'text/plain');
-        res.json(parsedData);
-        // Clean up: Close and remove the temporary file
-        await tmpFile.cleanup();
-
-
-    } catch (error) {
-        console.error('Backend Error:', error.message);
-        res.status(500).json({error: 'Error transcribing audio.', details: error.message});
-    }
-
-
+    const parsedData = JSON.parse(
+      JSON.parse(completion.choices[0].message.content)
+    );
+    console.log(parsedData);
+    console.log(transcript);
+    res.set('Content-Type', 'text/plain');
+    res.json({
+      original: transcript.text,
+      transcription: completion.choices[0].message.content,
+    });
+    // Clean up: Close and remove the temporary file
+    await tmpFile.cleanup();
+  } catch (error) {
+    console.error('Backend Error:', error.message);
+    res
+      .status(500)
+      .json({ error: 'Error transcribing audio.', details: error.message });
+  }
 });
 
 app.use('/mongo', mongoRoute);
 // IMPLEMENT GPT
-
-
-
-
-
--- frontend/src/components/VoiceRecord.jsx
+++ frontend/src/components/VoiceRecord.jsx
@@ -1,85 +1,109 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef } from 'react';
 import '../css/Client.css';
 
 import voice from '../assets/voice.svg';
 import stopRec from '../assets/stopRec.svg';
 
-
 function VoiceRecorder() {
-    const [recording, setRecording] = useState(false);
-    const [transcript, setTranscript] = useState(null);
-    const mediaRecorderRef = useRef(null);
-    const audioChunksRef = useRef([]);
-    const [caseInfo, setCaseInfo] = useState(null)
+  const [recording, setRecording] = useState(false);
+  const [transcript, setTranscript] = useState(null);
+  const mediaRecorderRef = useRef(null);
+  const audioChunksRef = useRef([]);
+  const [caseInfo, setCaseInfo] = useState(null);
 
-    const startRecording = async () => {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        mediaRecorderRef.current = new MediaRecorder(stream);
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorderRef.current = new MediaRecorder(stream);
 
-        mediaRecorderRef.current.onstart = () => {
-            audioChunksRef.current = [];
-        };
-
-        mediaRecorderRef.current.ondataavailable = (event) => {
-            audioChunksRef.current.push(event.data);
-        };
-
-        mediaRecorderRef.current.onstop = async () => {
-            const audioBlob = new Blob(audioChunksRef.current, { type: "audio/wav" });
-            const formData = new FormData();
-            formData.append('audio', audioBlob);
-
-            try {
-                 await fetch('http://localhost:4000/transcribe', {
-                    method: 'POST',
-                    body: formData
-                }).then(response => response.json())
-                     .then(data => {
-                         console.log(data.original);
-                         setTranscript(data.original);
-                         setCaseInfo(date.transcript)
-                     })
-                     .catch(error => {
-                         console.error('Error:', error);
-                     });
-            } catch (error) {
-                console.error("Error transcribing:", error.message);
-            }
-        };
-
-        mediaRecorderRef.current.start();
-        setRecording(true);
+    mediaRecorderRef.current.onstart = () => {
+      audioChunksRef.current = [];
     };
 
-    const stopRecording = () => {
-        if (mediaRecorderRef.current) {
-            mediaRecorderRef.current.stop();
-            setRecording(false);
-        }
+    mediaRecorderRef.current.ondataavailable = (event) => {
+      audioChunksRef.current.push(event.data);
     };
 
-    return (
-        <div> 
-            {recording ? <img src={stopRec} alt="voice-mic" className="leftTopImage"  onClick={() => stopRecording()} disabled={!recording}></img> : 
-            <img src={voice} alt="voice-mic" className="leftTopImage"  onClick={() => startRecording()} disabled={!recording}/>}
-            <div className="voice-container">
-                {transcript ? <p>{transcript}</p> : <p>Click the Mic Button to Start Recording</p>}
-                <div className="insideBox"></div>
-            </div>
-            <div class="right-half">
-                <h2 className="checklist">Please include the following information in your case description: </h2>
-                <ul>
-                    <li>First Name</li>
-                    <li>Last Name</li>
-                    <li>Phone Number</li>
-                    <li>Zip Code</li>
-                    <li>Email Address</li>
-                    <li>Case Type</li>
-                    <li>Case Description</li>
-                </ul>
-            </div>
+    mediaRecorderRef.current.onstop = async () => {
+      const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/wav' });
+      const formData = new FormData();
+      formData.append('audio', audioBlob);
+
+      try {
+        await fetch('http://localhost:4000/transcribe', {
+          method: 'POST',
+          body: formData,
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data.original);
+            setTranscript(data.original);
+            setCaseInfo(data.transcript);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+      } catch (error) {
+        console.error('Error transcribing:', error.message);
+      }
+    };
+
+    mediaRecorderRef.current.start();
+    setRecording(true);
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+      setRecording(false);
+    }
+  };
+
+  return (
+    <div className="topVoice">
+      <div className="leftSide">
+        {recording ? (
+          <img
+            src={stopRec}
+            alt="voice-mic"
+            className="leftTopImage"
+            onClick={() => stopRecording()}
+            disabled={!recording}
+          ></img>
+        ) : (
+          <img
+            src={voice}
+            alt="voice-mic"
+            className="leftTopImage"
+            onClick={() => startRecording()}
+            disabled={!recording}
+          />
+        )}
+        <div className="voice-container">
+          {transcript ? (
+            <p className="outText">{transcript}</p>
+          ) : (
+            <p className="mic">Tap mic icon to start recording</p>
+          )}
+          <div className="insideBox"></div>
         </div>
-    );
+      </div>
+
+      <div class="right-half">
+        <h2 className="checklist">
+          Ensure your case description<br></br> includes the following details
+        </h2>
+        <ul>
+          <li># First Name</li>
+          <li># Last Name</li>
+          <li># Phone Number</li>
+          <li># Zip Code</li>
+          <li># Email Address</li>
+          <li># Case Type</li>
+          <li># Case Description</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default VoiceRecorder;
